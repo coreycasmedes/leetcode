89a859b5ed9c0637e9959976df139a265acfc263
Corey Casmedes <ccasmedes3@gmail.com>
2024-08-01 14:12:47 -0500

    Time: 115 ms (77.93%), Space: 16.9 MB (64.83%) - LeetHub

--- 1129-longest-string-chain/1129-longest-string-chain.py
+++ 1129-longest-string-chain/1129-longest-string-chain.py
@@ -9,7 +9,8 @@ class Solution:
             for i in range(len(word)):
                 # find predeccesor by removing single letters from word
                 if word[:i] + word[i+1:] in dp:
-                    dp[word] = max(1, dp[word], dp[word[:i] + word[i+1:]] + 1)
+                    dp[word] = max(dp[word[:i] + word[i+1:]] + 1, dp[word])
+                    #dp[word] = max(1, dp[word], dp[word[:i] + word[i+1:]] + 1)
                     longest_chain = max(dp[word], longest_chain)
 
         
